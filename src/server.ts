961f405af06038a63a8ab12b74df19a08aa3afb4
hasibul1670 <hasibulislam1670@gmail.com>
2023-06-01 01:37:23 +0600

    ss

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import app from './app'
 import { connectDB } from './config/db'
 import { serverPort } from './secret'
 
+
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:${serverPort}`)
   await connectDB()
