e5f3230062bb7a5c7ceac86475baf5fc0bafb96c
hasibul1670 <hasibulislam1670@gmail.com>
2023-06-01 11:13:41 +0600

    husky

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,41 @@
-import express, { Express, NextFunction, Request, Response } from "express";
-import rateLimit from "express-rate-limit";
-import createHttpError from "http-errors";
-import morgan from "morgan";
+import express, { Express, NextFunction, Request, Response } from 'express'
+import rateLimit from 'express-rate-limit'
+import createHttpError from 'http-errors'
+import morgan from 'morgan'
 
-
-            const app: Express = express();
+const app: Express = express()
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 15 minutes
   max: 100,
-  message: "Too many requests from this IP. Please try again later",
+  message: 'Too many requests from this IP. Please try again later',
   standardHeaders: true,
   legacyHeaders: false,
-});
+})
 
-app.use(limiter);
+app.use(limiter)
 
-app.use(express.json());
+app.use(express.json())
 
+app.use(express.urlencoded({ extended: true }))
+app.use(limiter)
 
-                app.use(express.urlencoded({ extended: true }));
-app.use(limiter);
+app.use(morgan('dev'))
 
-app.use(morgan("dev"));
+app.get(
+  '/',
+  (
+    req: Request,
 
-
-
-
-                         app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Ejournal Server");
-});
-
+    res: Response
+  ) => {
+    res.send('Welcome to Ejournal Server')
+  }
+)
 
 //client error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createHttpError(404, "Route not found !!  404 😭"));
-});
+  next(createHttpError(404, 'Route not found !!  404 😭'))
+})
 
-
-
-
-
-export default app;
+export default app
